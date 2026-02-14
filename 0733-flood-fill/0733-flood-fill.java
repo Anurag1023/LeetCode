c302b69d394fa72e37e203c978e8d7a89455bcea
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-02-14 16:26:46 +0530

    Time: 0 ms (100%), Space: 46.8 MB (62.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,35 +1,22 @@
 class Solution {
+    public void dfs(int [][] image, int i,int j, int newcolor, int orcolor){
+        if(i<0 || j<0 || i >= image.length || j >= image[0].length ||
+        image[i][j]!=orcolor || image[i][j]==newcolor){
+            return;
+        } 
+        
+        image[i][j] = newcolor;
+
+
+        dfs(image,i-1,j,newcolor,orcolor);//top
+        dfs(image,i,j+1,newcolor,orcolor);//right
+        dfs(image,i+1,j,newcolor,orcolor);//bottom
+        dfs(image,i,j-1,newcolor,orcolor);//left
+
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-        
-        int originalColor = image[sr][sc];
-        
-        // Edge case
-        if (originalColor == color) return image;
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr, sc});
-        image[sr][sc] = color;
-        
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            
-            for (int[] dir : directions) {
-                int r = curr[0] + dir[0];
-                int c = curr[1] + dir[1];
-                
-                if (r >= 0 && r < n && c >= 0 && c < m 
-                    && image[r][c] == originalColor) {
-                    
-                    image[r][c] = color;
-                    q.offer(new int[]{r, c});
-                }
-            }
-        }
-        
+        dfs(image, sr, sc, color, image[sr][sc]);
         return image;
+        
     }
 }
