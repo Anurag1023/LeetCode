17d0c508ad624365f1b2f324793429e5ba79293d
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-02-22 14:15:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.06%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int binaryGap(int n) {
+        int max = 0;
+        int last = -1;
+        int pos = 0;
+
+        while(n > 0){
+            if((n & 1) == 1){
+                if(last != -1){
+                    max = Math.max(max, pos - last);
+                }
+                last = pos;
+            }
+            pos++;
+            n >>= 1;
+        }
+        return max;
+    }
+}
