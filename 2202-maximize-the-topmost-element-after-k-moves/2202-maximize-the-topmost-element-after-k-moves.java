b751735d85c1ade661e3725b3867dc4a60d22b56
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-03-19 14:21:54 +0530

    Time: 1 ms (91.96%), Space: 78.5 MB (82.91%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -1,22 +1,11 @@
 class Solution {
     public int maximumTop(int[] nums, int k) {
-        int n = nums.length;
-
-        if (n == 1) {
-            return (k % 2 == 0) ? nums[0] : -1;
-        }
-
-        if (k == 0) return nums[0];
-
-        int max = 0;
-
-        for (int i = 0; i < Math.min(n, k - 1); i++) {
+        if ( nums.length == 1 ) return k % 2 == 0 ? nums[0] : -1;
+        
+        int max = -1;
+        for ( int i = 0; i < nums.length && i < k-1; i++ )
             max = Math.max(max, nums[i]);
-        }
-
-        if (k < n) {
-            max = Math.max(max, nums[k]);
-        }
+        if ( k < nums.length ) max = Math.max(max, nums[k]);
 
         return max;
     }
