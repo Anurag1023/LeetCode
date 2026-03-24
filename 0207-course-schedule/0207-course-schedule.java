fd31e0dc5c2d5828997a8c263c5a805fbc759a32
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-03-24 16:43:09 +0530

    Time: 7 ms (60.42%), Space: 47.6 MB (11.15%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,46 +1,33 @@
 class Solution {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++)
-            adj.add(new ArrayList<>());
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
 
-        // Build graph
-        for (int[] pre : prerequisites)
-            adj.get(pre[1]).add(pre[0]);
-
-        int[] state = new int[numCourses];
-        // 0 = unvisited
-        // 1 = visiting
-        // 2 = visited
-
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (hasCycle(adj, state, i))
-                    return false;
-            }
+        for(int[] it: prerequisites){
+            adj.get(it[1]).add(it[0]);
+            indegree[it[0]]++;
         }
 
-        return true;
-    }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> res =  new ArrayList<>();
 
-    private boolean hasCycle(ArrayList<ArrayList<Integer>> adj, int[] state, int node) {
-
-        state[node] = 1; // visiting
-
-        for (int neighbor : adj.get(node)) {
-
-            if (state[neighbor] == 1)
-                return true; // cycle found
-
-            if (state[neighbor] == 0) {
-                if (hasCycle(adj, state, neighbor))
-                    return true;
-            }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.offer(i);
         }
 
-        state[node] = 2; // done
-        return false;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.offer(it);
+            }
+
+            res.add(node);
+        }
+
+        return res.size()==numCourses;
     }
 }
