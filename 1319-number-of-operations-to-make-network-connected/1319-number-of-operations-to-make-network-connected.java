a35f39bc7c2762b43afd0ebf5cf738816af7912d
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-04-22 23:09:07 +0530

    Time: 13 ms (36.2%), Space: 74 MB (11.64%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1;
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] conn : connections) {
+            graph.get(conn[0]).add(conn[1]);
+            graph.get(conn[1]).add(conn[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(graph, visited, i);
+                components++;
+            }
+        }
+
+        return components - 1;
+    }
+
+    private void dfs(List<List<Integer>> graph, boolean[] visited, int node) {
+        visited[node] = true;
+        for (int nei : graph.get(node)) {
+            if (!visited[nei]) {
+                dfs(graph, visited, nei);
+            }
+        }
+    }
+}
