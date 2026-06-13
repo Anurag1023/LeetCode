e17bdb1fcb18138947fd702be414479fd48fe3f4
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-14 03:49:08 +0530

    Time: 7 ms (13.11%), Space: 47.4 MB (6.56%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        String ans = "";
+        for(String word: words){
+            int sum = 0;
+            for(char c: word.toCharArray()){
+                sum += weights[c-'a'];
+            }
+
+            int mod = sum%26;
+            ans = ans + (char)('a'+25-mod);
+        }
+        return ans;
+    }
+}
