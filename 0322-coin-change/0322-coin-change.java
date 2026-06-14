02707d2751def56a2d6a5d88938309375d749cae
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-15 03:55:19 +0530

    Time: 27 ms (26.12%), Space: 47.1 MB (23.59%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+
+        for(int[] arr: dp){
+            Arrays.fill(arr,-1);
+        }
+        
+        int ans = helper(coins,amount,n-1,dp);
+        return ans==(int)1e9?-1:ans;
+    }
+
+    private int helper(int[] nums, int target, int ind, int[][] dp){
+        if(target==0) return 0;
+
+        if(ind==0){
+            if(target%nums[ind]==0){
+                return target/nums[ind];
+            }
+            else return (int)1e9;
+        }
+
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int take = (int)1e9;
+        if(target>=nums[ind]) take = 1 + helper(nums,target-nums[ind], ind,dp);
+        int notTake = helper(nums,target, ind-1,dp);
+        return dp[ind][target] = Math.min(take, notTake);
+    }
+}
