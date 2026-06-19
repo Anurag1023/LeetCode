cdd667718649f33d00d5ed6c8f334b9b753c8db6
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-20 05:03:16 +0530

    Time: 17 ms (30.04%), Space: 57 MB (31.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,30 +1,36 @@
 class Solution {
-
-    private int helper(int amount, int[] coins, int ind, int[][] dp ){
-        if(amount==0) return dp[ind][amount]=1;
-
-        if(ind==0){
-            if(amount%coins[0]==0) return dp[ind][amount]=1;
-            else return 0;
-        }
-
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-
-        int notTake = helper(amount, coins, ind-1,dp);
-        int take = 0;
-        if(coins[ind]<=amount) take = helper(amount-coins[ind],coins,ind,dp);
-
-        return dp[ind][amount] = take + notTake;
-    }
-
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
 
-        for(int[] a: dp){
-            Arrays.fill(a,-1);
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[0][t] = 1;
+            }
         }
 
-        return helper(amount,coins,n-1,dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+
+                if(j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                if(i==0){
+                    if(j%coins[0]==0) dp[i][j]=1;
+                    else  dp[i][j] = 0;
+                }
+
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(coins[i]<=j) take = dp[i][j-coins[i]];
+
+                dp[i][j] = take + notTake;                
+
+            }
+        }
+
+        return dp[n-1][amount];
     }
 }
