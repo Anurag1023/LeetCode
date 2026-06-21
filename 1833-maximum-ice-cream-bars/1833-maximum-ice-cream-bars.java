93cdd8a16ee8d424bf8da394f683e739ac2a1d2b
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-21 06:46:17 +0530

    Time: 210 ms (5.15%), Space: 96.7 MB (5.04%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,14 +1,25 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int cnt=0;
-        for(int i=0;i<costs.length;i++){
-            if(coins<costs[i]) return cnt;
-            else{
-                cnt++;
-                coins -= costs[i];
-            }
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+
+        for (int cost : costs) {
+            mp.put(cost, mp.getOrDefault(cost, 0) + 1);
         }
+
+        int cnt = 0;
+
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            int cost = entry.getKey();
+            int freq = entry.getValue();
+
+            int canBuy = Math.min(freq, coins / cost);
+
+            cnt += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) break;
+        }
+
         return cnt;
     }
 }
