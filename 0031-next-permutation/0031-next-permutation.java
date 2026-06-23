3bb4058fde2004569ea03a00e1d18b3c8a221ec8
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-24 03:14:37 +0530

    Time: 0 ms (100%), Space: 44.8 MB (15.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -20,17 +20,26 @@ class Solution {
                 left++;
                 right--;
             }
-        }else{
+        } else {
 
-            for(int i=n-1;i>dip;i--){
-                if(nums[i]>nums[dip]){
+            for (int i = n - 1; i > dip; i--) {
+                if (nums[i] > nums[dip]) {
                     int temp = nums[i];
                     nums[i] = nums[dip];
                     nums[dip] = temp;
                     break;
                 }
             }
-            Arrays.sort(nums,dip+1,n);
+
+            int left = dip + 1;
+            int right = n - 1;
+            while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
         }
     }
 }
