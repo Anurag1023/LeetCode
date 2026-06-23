b327edeca15857d7c70b6079cac1409ed6882d45
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-24 03:13:00 +0530

    Time: 2 ms (8.12%), Space: 44.8 MB (15.12%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int dip = -1;
+        int n = nums.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                dip = i;
+                break;
+            }
+        }
+
+        if (dip == -1) {
+            int left = 0, right = n - 1;
+
+            while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }else{
+
+            for(int i=n-1;i>dip;i--){
+                if(nums[i]>nums[dip]){
+                    int temp = nums[i];
+                    nums[i] = nums[dip];
+                    nums[dip] = temp;
+                    break;
+                }
+            }
+            Arrays.sort(nums,dip+1,n);
+        }
+    }
+}
