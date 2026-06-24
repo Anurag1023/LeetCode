ef86f8dfb1635d1d58766f931394f576221158c6
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-25 00:25:25 +0530

    Time: 2 ms (99.94%), Space: 52.6 MB (39.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } 
+            else if (num == candidate2) {
+                count2++;
+            } 
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } 
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } 
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
         List<Integer> ans = new ArrayList<>();
 
-        for(int num: nums){
-            hm.put(num,hm.getOrDefault(num,0)+1);
-        }
+        int limit = nums.length / 3;
 
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if(entry.getValue() > nums.length/3) ans.add(entry.getKey());
-        }
+        if (count1 > limit) ans.add(candidate1);
+        if (count2 > limit) ans.add(candidate2);
+
         return ans;
     }
 }
