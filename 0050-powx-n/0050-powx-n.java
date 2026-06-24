3b0f39660ae770dbced1894e239a959106944684
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-25 00:05:10 +0530

    Time: 0 ms (100%), Space: 47.9 MB (56.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N = n;
+        if(n<0){
+            N= N*-1;
+            x = 1/x;
+        }
+        return power(x,N);
+    }
+
+    private double power(double x, double n){
+        if(n==0) return 1;
+        else if(n%2==0) return power(x*x,n/2);
+        else return x*power(x*x, (n-1)/2);
     }
 }
