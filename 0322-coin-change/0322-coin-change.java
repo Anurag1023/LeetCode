a7305b525358dc15dbbcdf561831d8c28f7affb3
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-06-30 13:50:26 +0530

    Time: 10 ms (99.79%), Space: 46.4 MB (60.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[amount+1][coins.length];
 
-        for(int[] d: dp){
-            Arrays.fill(d,-1);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, (int)1e9);
+
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int target = coin; target <= amount; target++) {
+                dp[target] = Math.min(dp[target],
+                                      1 + dp[target - coin]);
+            }
         }
 
-        int cnt = helper(coins,amount,coins.length-1,dp);
-        return dp[amount][coins.length-1]>=(int)1e9?-1:dp[amount][coins.length-1];
+        return dp[amount] >= (int)1e9 ? -1 : dp[amount];
     }
-
-    private int helper(int[] coins, int amount,int ind,int[][] dp){
-        if(ind==0){
-            if(amount%coins[ind]==0){
-                return dp[amount][ind]= amount/coins[ind];
-            }else return dp[amount][ind] = (int)1e9;
-        }
-
-        if(dp[amount][ind]!=-1) return dp[amount][ind];
-
-        int take = (int)1e9;
-        if(coins[ind]<=amount) take = 1 + helper(coins,amount-coins[ind],ind,dp);
-
-        int notTake = helper(coins,amount,ind-1,dp);
-        return dp[amount][ind]= Math.min(take, notTake);
-    }
- }
+}
