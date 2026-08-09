2822d007dd064d4437bf503a48da3267810e6e16
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-08-09 06:15:25 +0530

    Time: 21 ms (94.26%), Space: 57.9 MB (19.67%) - LeetHub

---
+++ 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getMaximumConsecutive(int[] coins) {
+        Arrays.sort(coins);
+        int max =0;
+
+        for(int coin: coins){
+            if(coin > max+1) break;
+            max+=coin;
+        }
+        return max+1;
+    }
+}
