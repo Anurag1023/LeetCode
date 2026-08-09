f04e2d86affde53e167e41053046d98dc26d313a
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-08-09 10:32:55 +0530

    Time: 6 ms (99.28%), Space: 49.7 MB (44.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,47 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        boolean[] visited = new boolean[strs.length];
-        List<List<String>> res = new ArrayList<>();
-
-        for(int i=0;i<strs.length; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            List<String> ans = new ArrayList<>();
-            ans.add(strs[i]);
-            for(int j=0;j<strs.length;j++){
-                if(!visited[j] && isAnagram(strs[i],strs[j])){
-                    ans.add(strs[j]);
-                    visited[j]=true;
-                }
+        HashMap<String,List<String>> map = new HashMap<>();
+                
+        for( String s : strs){
+            char [] arr= s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            
+            if(map.containsKey(key)){
+                map.get(key).add(s);
+                
+            } else {
+                List<String> list= new ArrayList<>();
+                list.add(s);
+                map.put(key,list);
             }
-            res.add(ans);
         }
-        return res;
-    }
-
-    public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> m1 = new HashMap<>();
-
-        if (s.length() != t.length())
-            return false;
-
-        for (int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            m1.put(a, m1.getOrDefault(a, 0) + 1);
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            char a = t.charAt(i);
-            if (!m1.containsKey(a))
-                return false;
-            m1.put(a, m1.get(a) - 1);
-        }
-
-        for (Integer val : m1.values()) {
-            if (val != 0)
-                return false;
-        }
-
-        return true;
+        return new ArrayList <>(map.values());
     }
 }
