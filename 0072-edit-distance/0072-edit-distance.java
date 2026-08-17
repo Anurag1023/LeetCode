70904811cbd3db3ff0a47476fdd71f8ddd0fe0d9
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-08-17 07:42:16 +0530

    Time: 5 ms (66.4%), Space: 47.1 MB (71.71%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,13 +3,30 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n + 1][m + 1];
 
-        for (int[] row : dp) {
-            java.util.Arrays.fill(row, -1);
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = i;
+        for (int i = 0; i < m + 1; i++)
+            dp[0][i] = i;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][ j - 1];
+                } else {
+
+                    int insert = dp[i][j - 1];
+                    int delete = dp[i - 1][j];
+                    int replace = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + Math.min(insert,
+                            Math.min(delete, replace));
+                }
+            }
         }
 
-        return helper(n, m, word1, word2, dp);
+        return dp[n][m];
     }
 
     private int helper(int i, int j, String s1, String s2, int[][] dp) {
@@ -22,19 +39,19 @@ class Solution {
             return i;
         }
 
-        if (dp[i-1][j-1] != -1) {
-            return dp[i-1][j-1];
+        if (dp[i - 1][j - 1] != -1) {
+            return dp[i - 1][j - 1];
         }
 
-        if (s1.charAt(i-1) == s2.charAt(j-1)) {
-            return dp[i-1][j-1] = helper(i - 1, j - 1, s1, s2, dp);
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            return dp[i - 1][j - 1] = helper(i - 1, j - 1, s1, s2, dp);
         }
 
         int insert = helper(i, j - 1, s1, s2, dp);
         int delete = helper(i - 1, j, s1, s2, dp);
         int replace = helper(i - 1, j - 1, s1, s2, dp);
 
-        return dp[i-1][j-1] = 1 + Math.min(insert,
-                                Math.min(delete, replace));
+        return dp[i - 1][j - 1] = 1 + Math.min(insert,
+                Math.min(delete, replace));
     }
 }
