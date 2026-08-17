7e01603d02a36121de8f6b3c10acbdd544b23558
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-08-17 07:30:23 +0530

    Time: 3 ms (98.22%), Space: 47.3 MB (35.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,38 +3,38 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
 
         for (int[] row : dp) {
             java.util.Arrays.fill(row, -1);
         }
 
-        return helper(n - 1, m - 1, word1, word2, dp);
+        return helper(n, m, word1, word2, dp);
     }
 
     private int helper(int i, int j, String s1, String s2, int[][] dp) {
 
-        if (i < 0) {
-            return j + 1;
+        if (i == 0) {
+            return j;
         }
 
-        if (j < 0) {
-            return i + 1;
+        if (j == 0) {
+            return i;
         }
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (dp[i-1][j-1] != -1) {
+            return dp[i-1][j-1];
         }
 
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return dp[i][j] = helper(i - 1, j - 1, s1, s2, dp);
+        if (s1.charAt(i-1) == s2.charAt(j-1)) {
+            return dp[i-1][j-1] = helper(i - 1, j - 1, s1, s2, dp);
         }
 
         int insert = helper(i, j - 1, s1, s2, dp);
         int delete = helper(i - 1, j, s1, s2, dp);
         int replace = helper(i - 1, j - 1, s1, s2, dp);
 
-        return dp[i][j] = 1 + Math.min(insert,
+        return dp[i-1][j-1] = 1 + Math.min(insert,
                                 Math.min(delete, replace));
     }
 }
