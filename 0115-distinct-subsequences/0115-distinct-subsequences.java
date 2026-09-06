873de90ae7d95e9df0b5d3c2add99d63c59a4e22
Anurag Singh <159601481+Anurag1023@users.noreply.github.com>
2026-09-06 12:48:15 +0530

    Time: 20 ms (53.25%), Space: 54.3 MB (50.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,28 +1,52 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()][t.length()];
-        for(int[] d: dp){
-            Arrays.fill(d,-1);
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
         }
-        return helper(s,t,0,0,dp);
+
+        for (int i = 0; i <= t.length(); i++) {
+            dp[s.length()][i] = 0;
+        }
+        
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][t.length()] = 1;
+        }
+
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = t.length() - 1; j >= 0; j--) {
+
+                int not = dp[i + 1][j];
+                int take = 0;
+                if (s.charAt(i) == t.charAt(j)) {
+                    take = dp[i + 1][j + 1];
+                }
+
+                dp[i][j] = take + not;
+            }
+        }
+
+        return dp[0][0];
     }
 
-    private int helper(String s, String t, int i, int j,int[][] dp){
-        
-        if(j==t.length()){
+    private int helper(String s, String t, int i, int j, int[][] dp) {
+
+        if (j == t.length()) {
             return 1;
         }
 
-        if(i==s.length()){
+        if (i == s.length()) {
             return 0;
         }
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        int not = helper(s,t,i+1,j,dp);
+        int not = helper(s, t, i + 1, j, dp);
         int take = 0;
-        if(s.charAt(i)==t.charAt(j)){
-            take = helper(s,t,i+1,j+1,dp);
+        if (s.charAt(i) == t.charAt(j)) {
+            take = helper(s, t, i + 1, j + 1, dp);
         }
 
         return dp[i][j] = take + not;
